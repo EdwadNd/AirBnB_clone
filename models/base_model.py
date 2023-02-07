e06d85fec470c5d380fbcd4e8838ec82e0cef3c8
EdwadNd <Edwardthina@gmail.com>
2023-02-07 11:41:49 +0000

    building

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" a class BaseModel that defines all common attributes/methods for other classes """
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """base class for airBnB models"""
+
+    def __init__(self, *args, **kwargs):
+        """Instances a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['updated_at'] = datetime.strptime(
+                kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(
+                kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = ((str(type(self)).split('.')[-1])).split('\'')[0]
+        return '[{}] ({}0 {]'.format(cls, self.id, self.__dict__)
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                           ((str(type(self))).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
