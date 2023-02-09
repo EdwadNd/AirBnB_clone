5c94b5eb82ea08ac48a7a41410f121c097d716b9
EdwadNd <Edwardthina@gmail.com>
2023-02-09 07:50:59 +0000

    base model console

--- console.py
+++ console.py
@@ -2,20 +2,36 @@
 """ console shell """
 import cmd
 import sys
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    """ comandline processor """
+    """ commandline interpreter forr HBNB """
     # determines prompt for interactive/non-interactive modes
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+    classes = {'BaseModel': BaseModel}
 
     def do_quit(self, command):
         """Quit command to exit the program"""
         exit()
 
     def do_EOF(self, line):
-        """ Method to exit the program"""
+        """ EOF command to exit the program"""
         return True
+
+    def do_create(self, argument):
+        """ command creates  new instance of classes """
+        if not argument:
+            print("** class name missing **")
+            return
+        elif argument not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        _instance = HBNBCommand.classes[argument]()
+        storage.save()
+        print(_instance.id)
+        storage.save()
 
 
 if __name__ == '__main__':
