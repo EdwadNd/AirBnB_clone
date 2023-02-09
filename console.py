8eff82d302408721ab04219c9941d70c34eea0b5
EdwadNd <Edwardthina@gmail.com>
2023-02-09 07:07:55 +0000

    console

--- console.py
+++ console.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 """ console shell """
-class HBNBCommand(cmd.Cmd):
+import cmd
+import sys
 
+
+class HBNBCommand(cmd.Cmd):
+    """ comandline processor """
+    # determines prompt for interactive/non-interactive modes
+    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+
+    def do_quit(self, command):
+        """Quit command to exit the program"""
+        exit()
+
+    def do_EOF(self, line):
+        """ Method to exit the program"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
